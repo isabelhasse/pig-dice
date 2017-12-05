ddc9b822f1111480066a16bed42acf22e1ba648d
Isabel Hasse and Robin Sola <robin@>
2017-12-04 19:05:32 -0800

    Add function that passes to next players turn when 1 is rolled.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,44 @@
+function Player(score) {
+  this.score = score;
+}
+
+var whoseTurn = 1;
+
+var player1 = new Player(0);
+var player2 = new Player(0);
+
 var rollDice = function() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
+Player.prototype.processRoll = function() {
+  var roll = rollDice()
+  if (roll === 1) {
+    nextPlayer();
+  }
+  $("#roll-result").text(roll);
+}
+
+var nextPlayer = function() {
+  if (whoseTurn === 1) {
+    whoseTurn = 2;
+  } else {
+    whoseTurn = 1;
+  }
+};
+
 $(document).ready(function(){
   $("#roll-dice").click(function() {
-    $("#result").text(rollDice());
-
+    if(whoseTurn === 1) {
+      player1.processRoll();
+    } else {
+      player2.processRoll();
+    }
+    //$("#roll-result").text(rollDice());
+    if (whoseTurn === 1) {
+      $("#whose-turn").html("<h3>Player 1</h3>");
+    } else {
+      $("#whose-turn").html("<h3>Player 2</h3>")
+    }
   });
 });
